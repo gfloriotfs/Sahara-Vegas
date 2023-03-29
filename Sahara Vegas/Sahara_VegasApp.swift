769f05b5b3693ad5f1cdf505ae6f43d5532dedb5
Graydon <gflorio@tfs.ca>
2023-03-29 14:35:39 -0400

    Initial Commit

---
+++ Sahara Vegas/Sahara_VegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sahara_VegasApp.swift
+//  Sahara Vegas
+//
+//  Created by Graydon Florio on 2023-03-29.
+//
+
+import SwiftUI
+
+@main
+struct Sahara_VegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
